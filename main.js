2444cbbec9d678e7100c18d7cce65b5977712579
Yogye Berry <yogyeberry@gmail.com>
2026-09-16 00:47:33 +0530

    Refactor showSongs function to showsongs and enhance audio file detection

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 const fs = require("fs");
 const path = require("path");
-function showSongs() {
+function showsongs() {
     const songsFolder = path.join(__dirname, "songs");
     const files = fs.readdirSync(songsFolder);
-    console.log("Files in songs folder:",'\n');
+    const songs = []
     for (let i = 0; i < files.length; i++) {
-        if (files[i].endsWith(".mp3")) {
-            console.log(i+1, files[i]);
+        if (files[i].endsWith(".mp3") || files[i].endsWith(".wav") || files[i].endsWith(".flac")) {
+            const fullPath = path.join(songsFolder, files[i]);
+            songs.push({
+                title: files[i],
+                path: fullPath,
+                duration: null,
+            });
         }
     }
+    return songs;
 }
-showSongs();
+const songs = showsongs();
+if (songs.length === 0) {
+    console.log("No supported audio files found.");
+} else {
+    console.log("Available songs:");
+    for (let i = 0; i < songs.length; i++) {
+        console.log(i + 1, songs[i].title);
+    }
+}
